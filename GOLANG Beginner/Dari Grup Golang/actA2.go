c5d2843ce0f5c3febe6fbe3734586e9fd186f808
Ali Akbar Said <142162298+12a3k4@users.noreply.github.com>
2024-01-12 21:06:16 +0700

    grup

---
+++ GOLANG Beginner/Dari Grup Golang/actA2.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var f_name string = "Muhammad Bayu"
+	l_name := "Prasetyo"
+	age := 19
+
+	fmt.Printf("My name is %s %s, I am %d old\n", f_name, l_name, age)
+}
